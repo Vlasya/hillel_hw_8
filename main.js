5cc8f3586ce83310d42bc55f405db6a08cbb5c14
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-09-13 22:01:44 +0300

    done

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-// Создать объект с такой структурой: obj = { x: 10, y: 20, inner: { x: 20, z: 30 }, foo2: { k: 23, p: 13 } } Написать функцию convert(obj), которая получает аргументом obj. Функция должна вернуть новый объект:
-// newObj = {
-// 	x: 20,
-// 	y: 20,
-// 	z: 30,
-// 	k: 23,
-// 	p: 13
-// }
+// Написать функцию assignObjects(obj1, obj2), которая принимает аргументами 2 объекта и возвращает новый, который состоит из свойств обоих объектов (склеить). Если свойство повторяется, то взять значение второго объекта
+
+// assignObjects({ x: 10, y: 20 }, { z: 30 }) -> { x:10, y:20, z: 30 }
+// assignObjects({ x: 10 }, { x: 20, y: 30 }) - > { x:20, y: 30 }
 
 
-let obj = {
-	x: 10, 
-	y: 20,
-	inner: 
-		{ x: 20, 
-		z: 30 },
-	foo2: 
-		{ k: 23,
-		  p: 13 } 
-		}
+let obj={x: 10,y: 20 };
+let obj1= {z: 30};
 
+let obj3 ={x: 10};
+let obj4={x: 20,y: 30}
 
-function convert (obj) {
-	newObj = {
-			x: 20,
-			y: 20,
-			z: 30,
-			k: 23,
-			p: 13
-		}
-		return newObj
+function assignObjects(obj1, obj2){
+	return Object.assign(obj1,obj2)
+ 	 
 }
 
-
-console.log('convert(obj): ', convert(obj));
+console.log('newObject(obj,obj2): ', assignObjects(obj3,obj4));
